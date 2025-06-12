e2e7c71925e70e1fa9465bd0391686a885d7c9f4
Nitin Sharma <nitin.20230035@mnnit.ac.in>
2025-06-12 17:10:01 +0530

    Compostion maded

--- composition.py
+++ composition.py
@@ -1,2 +1,12 @@
 def Compostion(k, Text):
-    
+    n = len(Text)
+    kmers = []
+    for i in range(n-k+1):
+        kmer = Text[i:i+k]
+        kmers.append(kmer)
+    sorted_kmers = sorted(kmers)
+    return sorted_kmers
+
+k = 5
+Text =  "CAATCCAAC"
+print(Compostion(k, Text))   
