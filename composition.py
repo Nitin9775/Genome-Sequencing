d226e8014fa0298c07ff6211813567f78863882a
Nitin Sharma <nitin.20230035@mnnit.ac.in>
2025-06-13 08:26:36 +0530

    now final

--- composition.py
+++ composition.py
@@ -4,8 +4,12 @@ def Compostion(k, Text):
     for i in range(n-k+1):
         kmer = Text[i:i+k]
         kmers.append(kmer)
-    return kmers
+        sorted_kmers = sorted(kmers)
+    return sorted_kmers
 
-k = 5
-Text =  "CAATCCAAC"
-print(Compostion(k, Text))   
+k = 100
+with open(r"C:\Users\LENOVO\Downloads\dataset_30153_3 (4).txt", "r") as file:
+    contents = file.read()         # Read the entire content
+    Text = contents
+
+print(*Compostion(k, Text))   
